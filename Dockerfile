73d2ee386a95a70dcd6b2a87539920ce543c78c9
I-am-PUID-0 <JBert@33w.io>
2023-04-13 16:35:45 -0400

    Local automatic update

--- Dockerfile
+++ Dockerfile
@@ -9,19 +9,20 @@ COPY --from=builder /go/rclone_RD-master/rclone /rclone-linux
 ADD https://github.com/itsToggle/plex_debrid/archive/refs/heads/main.zip /
 ADD setup.sh /
 ADD pd_setup.py /
+ADD update.py /
 ADD settings-default.json /
 ADD healthcheck.sh /
 ENV \
   XDG_CONFIG_HOME=/config \
   TERM=xterm 
 RUN \
-  apk add --update --no-cache tzdata ca-certificates wget fuse python3 build-base py3-pip python3-dev py3-dotenv && ln -sf python3 /usr/bin/python && \
+  apk add --update --no-cache tzdata ca-certificates wget fuse python3 build-base py3-pip python3-dev py3-dotenv py3-schedule && ln -sf python3 /usr/bin/python && \
   mkdir /config && touch /config/ignored.txt && \     
   chmod 711 rclone-linux && chmod 755 /setup.sh && chmod +x healthcheck.sh &&\
   unzip main.zip && rm main.zip && \
-  mv plex_debrid-main/ plex_debrid && rm /plex_debrid/README.md && rm /plex_debrid/Dockerfile && rm -R /plex_debrid/.github && \  
+  mv plex_debrid-main/ plex_debrid && rm /plex_debrid/README.md && rm /plex_debrid/Dockerfile && rm -R /plex_debrid/.github && \   
   pip3 install -r /plex_debrid/requirements.txt 
 HEALTHCHECK --interval=60s --timeout=10s \
   CMD ./healthcheck.sh  || exit 1
 VOLUME /config  
-CMD ./setup.sh;/bin/sh
+CMD ["python", "/update.py"];/bin/sh
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-echo "Setting up rclone_RD"
+echo "Configuring rclone_RD"
 umount /data/$RCLONE_MOUNT_NAME || true 2>/dev/null
 mkdir -p /data/$RCLONE_MOUNT_NAME
 touch /config/rclone.config
@@ -12,23 +12,24 @@ EOF
 echo "user_allow_other" >> /etc/fuse.conf
 echo "Checking rclone_RD flags"
 if [[ -n "$RCLONE_MOUNT_NAME" ]];
-then echo "Setting the rclone mount name to $RCLONE_MOUNT_NAME";
+then echo "Configuring the rclone mount name to $RCLONE_MOUNT_NAME";
 else echo "Please set a name for the rclone mount" && exit 1;
 fi
 if [[ -n "$RD_API_KEY" ]];
-then echo "Setting the API Key";
+then echo "Configuring the API Key";
 else echo "Please set the API Key for the rclone mount" && exit 1;
 fi
 echo "Starting rclone_rd"
-./rclone-linux mount "$RCLONE_MOUNT_NAME": /data/"$RCLONE_MOUNT_NAME" --config /config/rclone.config --allow-other --daemon
-echo "Setting up plex_debrid"
+/rclone-linux mount "$RCLONE_MOUNT_NAME": /data/"$RCLONE_MOUNT_NAME" --config /config/rclone.config --allow-other --daemon
+echo "Configuring plex_debrid"
 FILE=/config/settings.json
 if [ ! -f "$FILE" ]; 
-then cp /settings-default.json /config/settings.json && python pd_setup.py
-else python pd_setup.py	
+then cp /settings-default.json /config/settings.json && python /pd_setup.py
+else python /pd_setup.py	
 fi
+echo "plex_debrid configuration complete"
 echo "To ensure that Plex can access the rclone_RD mount, please restart Plex now"
-echo "The script will wait till the reboot is complete"
+echo "The script will wait till the restart is complete"
 sleep 30
 sp='/-\|'
 printf ' '
@@ -41,4 +42,3 @@ echo
 echo "Waiting 30s for Plex to finish starting"
 sleep 30
 echo "Starting plex_debrid"
-python /plex_debrid/main.py --config-dir /config
